91980fd0d73af8b688503e81f44feaea91c9a040
PAUL WONG (INFRA-ISD-OOCLL/HKG) <WONGPA@OOCL.COM>
2020-04-14 15:08:33 +0800

    Rename and remove zombie code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,58 +7,57 @@ public class WordFrequencyGame {
     public static final String SPACE_DELIMITER = " ";
     public static final String CALCULATE_ERROR = "Calculate Error";
 
-    public String getResult(String inputStr) {
+    public String getResult(String sentence) {
 
 
-        if (inputStr.split(SPACE_PATTERN).length == 1) {
-            return inputStr + " 1";
+        if (sentence.split(SPACE_PATTERN).length == 1) {
+            return sentence + " 1";
         } else {
 
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split(SPACE_PATTERN);
+                String[] wordArray = sentence.split(SPACE_PATTERN);
 
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
+                List<WordInfo> wordInfoList = new ArrayList<>();
+                for (String word : wordArray) {
+                    WordInfo wordInfo = new WordInfo(word, 1);
+                    wordInfoList.add(wordInfo);
                 }
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
+                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
 
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
+                List<WordInfo> list = new ArrayList<>();
+                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
+                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+                    list.add(wordInfo);
                 }
-                inputList = list;
+                wordInfoList = list;
 
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
                 StringJoiner joiner = new StringJoiner(NEXT_LINE_DELIMITER);
-                for (Input w : inputList) {
-                    String s = w.getValue() + SPACE_DELIMITER + w.getWordCount();
-                    joiner.add(s);
+                for (WordInfo wordinfo : wordInfoList) {
+                    String wordWithCount = wordinfo.getWord() + SPACE_DELIMITER + wordinfo.getWordCount();
+                    joiner.add(wordWithCount);
                 }
                 return joiner.toString();
-            } catch (Exception e) {
+            } catch (Exception exception) {
                 return CALCULATE_ERROR;
             }
         }
     }
 
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
+    private Map<String, List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
+        Map<String, List<WordInfo>> map = new HashMap<>();
+        for (WordInfo wordInfo : wordInfoList) {
+            if (!map.containsKey(wordInfo.getWord())) {
+                ArrayList wordinfoList = new ArrayList<>();
+                wordinfoList.add(wordInfo);
+                map.put(wordInfo.getWord(), wordinfoList);
             } else {
-                map.get(input.getValue()).add(input);
+                map.get(wordInfo.getWord()).add(wordInfo);
             }
         }
         return map;
---
+++ src/main/java/WordInfo.java
@@ -0,0 +1,17 @@
+public class WordInfo {
+    private String word;
+    private int wordCount;
+
+    public WordInfo(String word, int wordCount) {
+        this.word = word;
+        this.wordCount = wordCount;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public int getWordCount() {
+        return this.wordCount;
+    }
+}
